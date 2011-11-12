de3e832cf37c4474ba56ed9fc5475bfcc028b043
Maaya Mizunashi <mmnashi90@gmail.com>
2011-11-12 13:10:27 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryTime::Application.config.secret_token = '51c8fa02ddf2d915edb7cc9e119f837323217850efe337aa0329dbd13614a44e2fe2fa53b65b029b4aff9be1f0837a2857e990d5905705ec56ab8b96484a27d6'
